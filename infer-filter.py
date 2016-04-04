439fc9df47bda26ffb4400586df03ebe6dc77fcf
Michael Comella <michael.l.comella@gmail.com>
2016-04-04 14:21:25 -0700

    Add infer-filter script.

---
+++ infer-filter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from itertools import islice
+import re
+import urllib2
+
+SERVER = 'https://people.mozilla.org/~sledru/reports/fennec-infer/bugs.txt'
+FILTERS = map(lambda s: re.compile(s), [
+    '^.+/mobile/android/tests/.+',
+    '^.+/mobile/android/thirdparty/.+',
+])
+
+
+def should_filter(line):
+    filtered = filter(lambda regex: regex.search(line), FILTERS)
+    return len(filtered) > 0
+
+
+line_iter = iter(urllib2.urlopen(SERVER))
+for line in line_iter:
+    if should_filter(line):
+        next(line_iter)  # Skip description
+        next(line_iter)  # Skip blank
+    else:
+        print line.replace('\n', '')
